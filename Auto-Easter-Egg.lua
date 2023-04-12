fda18c03adfcd10354951625ee8389824889d1a3
Hima <99522549+Levi4Chan@users.noreply.github.com>
2023-04-12 21:10:55 +0800

    Create Auto-Easter-Egg.lua

---
+++ Auto-Easter-Egg.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Levi4Chan/Rblx-Scripts/main/PSX-Network-Bypass.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Levi4Chan/Rblx-Scripts/main/Auto-Egg-Hunt-PSX.lua"))()
